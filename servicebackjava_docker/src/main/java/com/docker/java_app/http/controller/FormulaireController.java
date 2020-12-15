c3aea344896cb84950860c2be5957fdce6ac7936
ClaudiaMatsiaze <tmatsiaze@gmail.com>
2020-12-15 15:56:55 +0100

    updates

--- servicebackjava_docker/src/main/java/com/docker/java_app/http/controller/FormulaireController.java
+++ servicebackjava_docker/src/main/java/com/docker/java_app/http/controller/FormulaireController.java
@@ -1,23 +1,19 @@
 package com.docker.java_app.http.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.docker.java_app.dao.FormulaireRepository;
+//import com.docker.java_app.dao.FormulaireRepository;
 import com.docker.java_app.model.Formulaire;
 
 @Controller
 //@RequestMapping(path="/dockergo")
 public class FormulaireController {
 
-	@Autowired
-	private FormulaireRepository formRepository;
+	//@Autowired
+//	private FormulaireRepository formRepository;
 	      
 	@GetMapping(path="/")
 	public String accueil(Model model) {
